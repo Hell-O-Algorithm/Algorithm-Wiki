9c4d74b394f7f20b34fb1df73b26a6d8e540cf9d
hansung0904 <hansung0904@naver.com>
2023-01-17 19:51:01 +0900

    update : refactor project

---
+++ Baekjoon/Hanseong/src/main/java/com/wiki/algorithm/baekjoon/stringquestion/Number2908.java
@@ -0,0 +1,21 @@
+package com.wiki.algorithm.baekjoon.stringquestion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Number2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int inputNumberOne = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int inputNumberTwo = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println(inputNumberOne > inputNumberTwo ? inputNumberOne : inputNumberTwo);
+        br.close();
+
+
+    }
+}
